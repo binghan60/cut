abfaf2833c0147ac328605ee9b044e3b76773632
binghan60 <binghan60@gmail.com>
2024-10-04 10:26:48 +0800

    修改month init

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ createApp({
 			month: new Date().getMonth() + 1,
 			day: new Date().getDate(),
 			currentYear: new Date().getFullYear(),
-			currentMonth: new Date().getMonth(),
+			currentMonth: new Date().getMonth() + 1,
 			weekdays: ['一', '二', '三', '四', '五', '六', '日'],
 			quickSugar: '',
 			quickInsulin: '',
